7a8c9ad6ba79a34cbfdfb2f9c16c2af729d7f302
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2015-05-27 21:39:46 +0200

    Initial Commit

---
+++ DTUSensors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DTUSensors
+//
+//  Created by Andres on 27/5/15.
+//  Copyright (c) 2015 Andres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DTUSensorsTests/DTUSensorsTests.swift
@@ -0,0 +1,36 @@
+//
+//  DTUSensorsTests.swift
+//  DTUSensorsTests
+//
+//  Created by Andres on 27/5/15.
+//  Copyright (c) 2015 Andres. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DTUSensorsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
